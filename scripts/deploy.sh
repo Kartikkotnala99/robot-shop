086ce6055e9df7fd99fd70147f6284b949ad8aa0
Ubuntu <ubuntu@ip-172-31-38-117.ap-south-1.compute.internal>
2026-02-05 07:09:17 +0000

    add codedeploy pipeline

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/ubuntu/robot-shop
+
+docker compose down
+docker compose pull
+docker compose up -d
+1~#!/bin/bash
+cd /home/ubuntu/robot-shop
+
+docker compose down
+docker compose pull
+docker compose up -d
